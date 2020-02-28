ac4fa33b09b482e77f0f34c4799d45244dc00acf
DDRKirby(ISQ) <ddrkirby@gmail.com>
2020-02-28 01:27:06 -0800

    New puzzle BGs (only using 8 for now)

--- puzzles-for-pod/scripts/PuzzleData.gd
+++ puzzles-for-pod/scripts/PuzzleData.gd
@@ -4,15 +4,71 @@ class_name PuzzleData
 
 const PUZZLES:Dictionary = {
 	"1": {
-		"image": preload("res://sprites/puzzle-images/fall_img.jpg"),
+		"image": preload("res://sprites/puzzle-images/1.jpg"),
 		"masks": "1",
 	},
 	"2": {
-		"image": preload("res://sprites/puzzle-images/ice_skate.jpg"),
+		"image": preload("res://sprites/puzzle-images/2.jpg"),
 		"masks": "1",
 	},
 	"3": {
-		"image": preload("res://sprites/puzzle-images/waterfall.jpg"),
+		"image": preload("res://sprites/puzzle-images/3.jpg"),
 		"masks": "1",
 	},
+	"4": {
+		"image": preload("res://sprites/puzzle-images/4.jpg"),
+		"masks": "1",
+	},
+	"5": {
+		"image": preload("res://sprites/puzzle-images/5.jpg"),
+		"masks": "1",
+	},
+	"6": {
+		"image": preload("res://sprites/puzzle-images/6.jpg"),
+		"masks": "1",
+	},
+	"7": {
+		"image": preload("res://sprites/puzzle-images/7.jpg"),
+		"masks": "1",
+	},
+	"8": {
+		"image": preload("res://sprites/puzzle-images/8.jpg"),
+		"masks": "1",
+	},
+	# "9": {
+	# 	"image": preload("res://sprites/puzzle-images/9.jpg"),
+	# 	"masks": "1",
+	# },
+	# "10": {
+	# 	"image": preload("res://sprites/puzzle-images/10.jpg"),
+	# 	"masks": "1",
+	# },
+	# "11": {
+	# 	"image": preload("res://sprites/puzzle-images/11.jpg"),
+	# 	"masks": "1",
+	# },
+	# "12": {
+	# 	"image": preload("res://sprites/puzzle-images/12.jpg"),
+	# 	"masks": "1",
+	# },
+	# "13": {
+	# 	"image": preload("res://sprites/puzzle-images/13.jpg"),
+	# 	"masks": "1",
+	# },
+	# "14": {
+	# 	"image": preload("res://sprites/puzzle-images/14.jpg"),
+	# 	"masks": "1",
+	# },
+	# "15": {
+	# 	"image": preload("res://sprites/puzzle-images/15.jpg"),
+	# 	"masks": "1",
+	# },
+	# "16": {
+	# 	"image": preload("res://sprites/puzzle-images/16.jpg"),
+	# 	"masks": "1",
+	# },
+	# "17": {
+	# 	"image": preload("res://sprites/puzzle-images/17.jpg"),
+	# 	"masks": "1",
+	# },
 }
