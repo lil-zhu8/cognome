72b371d2f8f8e35cd24e0f7207a12b50cf529d62
DDRKirby(ISQ) <ddrkirby@gmail.com>
2020-02-29 03:24:34 -0800

    Use music note for music button, use top-right of device

--- puzzles-for-pod/scripts/MusicPlayer.gd
+++ puzzles-for-pod/scripts/MusicPlayer.gd
@@ -4,6 +4,8 @@ func _ready() -> void:
 	var enabled:bool = SaveData.Get("music_enabled", true)
 	var player:AudioStreamPlayer = $AudioStreamPlayer
 	player.playing = enabled
+	var button:TextureButton = $Button
+	button.modulate = Color.white if enabled else Color(1.0, 1.0, 1.0, 0.5)
 
 func OnButtonPressed() -> void:
 	var enabled:bool = SaveData.Get("music_enabled", true)
@@ -13,5 +15,7 @@ func OnButtonPressed() -> void:
 	player.stream_paused = !enabled
 	if enabled && !player.playing:
 		player.playing = true
-
+	var button:TextureButton = $Button
+	button.modulate = Color.white if enabled else Color(1.0, 1.0, 1.0, 0.5)
+	
 
