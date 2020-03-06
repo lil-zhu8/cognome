7b9fb8e2bbe9410ee7547c2c85865d775d549e1a
DDRKirby(ISQ) <ddrkirby@gmail.com>
2020-03-06 15:15:16 -0800

    temp screenshot branch

--- puzzles-for-pod/scripts/MusicPlayer.gd
+++ puzzles-for-pod/scripts/MusicPlayer.gd
@@ -18,4 +18,12 @@ func OnButtonPressed() -> void:
 	var button:TextureButton = $Button
 	button.modulate = Color.white if enabled else Color(1.0, 1.0, 1.0, 0.5)
 	
-
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_K:
+		print('awftnaw222222222222fuytnawfuyt')
+		var img = get_viewport().get_texture().get_data()
+		yield(get_tree(), "idle_frame")
+		yield(get_tree(), "idle_frame")
+		img.flip_y()
+		img.save_png("user://cap.png")
+	
