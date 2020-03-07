581ccc70c0d4269c701a2d5a38d1bb993945d28d
Timmie Wong <timmie.wong@wbgames.com>
2020-03-06 16:12:39 -0800

    Update version to 1.04

--- puzzles-for-pod/analytics/analytics.gd
+++ puzzles-for-pod/analytics/analytics.gd
@@ -175,7 +175,7 @@ func _ready() -> void:
 	var sessionNum:int = SaveData.Get("session_number", 1)
 	SaveData.Set("session_number", sessionNum + 1)
 	set_key('user_id', userId)
-	set_key('game_version', '1.03')
+	set_key('game_version', '1.04')
 	set_key('session_num', sessionNum)
 	request_init()
 	add_to_event_queue(get_user_event())
